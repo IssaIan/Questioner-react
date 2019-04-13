e5477847262a835e7b791fca7b476edb6b93a5f6
Issa Maina <issamaina@Issas-MacBook-Pro.local>
2019-04-13 22:29:02 +0300

    updates

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -3,13 +3,13 @@ import { BeatLoader } from "react-spinners";
 
 /** function component to render a circular spinner */
 const Loader = () => (
-    
+
       <div id="loader-body" className="loader-body">
         <div className="sweet-loading" />
         <div align="center" className="lds-ripple">
           <BeatLoader
             sizeUnit="px"
-            size={60}
+            size={6}
             color="#fafafa"
           />
         </div>
