a0f89a2bd09133eb8ff37314035262c16d708878
Issa Maina <issamaina@Issas-MacBook-Pro.local>
2019-04-14 23:00:42 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 var path = require('path');
 
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static("src"));
 app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'src/index.html'));
+    res.sendFile(path.resolve(__dirname, "src", "index.html"));
 });
 app.listen(process.env.PORT || 8080);
